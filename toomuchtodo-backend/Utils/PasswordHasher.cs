1b4d282b5008f535471b1675af35570846b1d171
fmacedosantos <felipemacedo986@gmail.com>
2025-02-16 20:38:30 -0300

    Criando o util PasswordHasher, registrando e verificando senhas hash

---
+++ toomuchtodo-backend/Utils/PasswordHasher.cs
@@ -0,0 +1,14 @@
+namespace toomuchtodo_backend.Utils;
+
+public static class PasswordHasher
+{
+    public static string HashPassword(string password)
+    {
+        return BCrypt.Net.BCrypt.HashPassword(password);
+    }
+
+    public static bool VerifyHashedPassword(string password, string hashedPassword)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+    }
+}
