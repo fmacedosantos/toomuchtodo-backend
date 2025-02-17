b4f7a142670e0cf6019bb75de03bd2fb12941f0d
fmacedosantos <felipemacedo986@gmail.com>
2025-02-17 00:02:16 -0300

    Criando os viewModels de tarefa

---
+++ toomuchtodo-backend/ViewModels/CreateTaskViewModel.cs
@@ -0,0 +1,7 @@
+namespace toomuchtodo_backend.ViewModels;
+
+public class CreateTaskViewModel
+{
+    public string Title { get; set; }
+    public string? Description { get; set; }
+}
---
+++ toomuchtodo-backend/ViewModels/UpdateTaskViewModel.cs
@@ -0,0 +1,7 @@
+namespace toomuchtodo_backend.ViewModels;
+
+public class UpdateTaskViewModel
+{
+    public string Title { get; set; }
+    public string? Description { get; set; }
+}
