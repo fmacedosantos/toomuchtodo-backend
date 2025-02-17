97260b994a20489f2bc895dbe1b0f77ed0b9f82c
fmacedosantos <felipemacedo986@gmail.com>
2025-02-16 22:21:36 -0300

    Adicionando a regra de implementação GetByUsername

--- toomuchtodo-backend/Models/IUserRepository.cs
+++ toomuchtodo-backend/Models/IUserRepository.cs
@@ -8,6 +8,8 @@ public interface IUserRepository
     
     User? GetByEmail(string email);
     
+    User? GetByUsername(string username);
+    
     void Update(User user);
     
     void Delete(User user);
