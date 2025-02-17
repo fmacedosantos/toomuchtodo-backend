382681dbed305f111a8e68ac950bcf3c7dc800a2
fmacedosantos <felipemacedo986@gmail.com>
2025-02-16 22:07:38 -0300

    Configurando o model de usuário para atendar à atualização de senha

--- toomuchtodo-backend/Models/User.cs
+++ toomuchtodo-backend/Models/User.cs
@@ -11,17 +11,25 @@ public class User
     public string username { get; private set; }
     public string email { get; private set; }
     public string password { get; private set; }
+    public DateTime updated_at { get; private set; }
 
     public User(string username, string email, string password)
     {
         this.username = username ?? throw new ArgumentNullException(nameof(username));
         this.email = email ?? throw new ArgumentNullException(nameof(email));
         this.password = password ?? throw new ArgumentNullException(nameof(password));
+        this.updated_at = DateTime.UtcNow;
     }
 
     public User(string email, string password)
     {
         this.email = email ?? throw new ArgumentNullException(nameof(email));
         this.password = password ?? throw new ArgumentNullException(nameof(password));
+    }
+
+    public void UpdatePassword(string newPasswordHash)
+    {
+        this.password = newPasswordHash ?? throw new ArgumentNullException(nameof(newPasswordHash));
+        this.updated_at = DateTime.UtcNow;
     }
 }
